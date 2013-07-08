29a236f2d26e81dd052c6934184a16c30a703112
Iñaki Abete <inakiabt@gmail.com>
2013-07-08 03:20:58 -0300

    Changed methods file not found error message

--- src/Etsy/EtsyApi.php
+++ src/Etsy/EtsyApi.php
@@ -21,7 +21,7 @@ class EtsyApi
 
 		if (!file_exists($methods_file))
 		{
-			exit("'{$methods}' not exists");
+			exit("Etsy methods file '{$methods}' not exists");
 		}
 		$this->methods = json_decode(file_get_contents($methods_file), true);
 		$this->client = $client;
