ee5f4828e86c92bd29b9deaa6b7291378307c35e
Iñaki Abete <inakiabt@gmail.com>
2013-07-08 17:08:29 -0300

    Testing post install scripts

---
+++ src/Etsy/PackageInstaller.php
@@ -0,0 +1,22 @@
+<?php
+namespace Etsy;
+
+use Composer\Script\Event;
+
+class PackageInstaller
+{
+    public static function postPackageInstall(Event $event)
+    {
+        $composer = $event->getComposer();
+
+        $config = $composer->getConfig();
+
+        $autoload_filepath = $config->get('vendor-dir') . '/autoload.php';
+
+        $destination_file  = dirname(realpath(__FILE__)) . '/../../scripts/autoload.php';
+
+        $contents = "<?php require_once('{$autoload_filepath}');";
+        
+        file_put_contents($destination_file, $contents);
+    }
+}
